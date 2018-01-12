cf71aea151a62a1915921bd743fb6d73f84bc58d
Frank Ngo <frankngomusic@gmail.com>
2018-01-12 10:43:39 -0800

    add form text for survey and basic js layout

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function)(){
+  $("#track_form").submit(function(event){
+    var question1 = $("input[name=question1]:checked").val();
+    var question2 = $("input[name=question2]:checked").val();
+    var question3 = $("input[name=question3]:checked").val();
+    var question4 = $("input[name=question4]:checked").val();
+    var question5 = $("input[name=question5]:checked").val();
+    var question6 = $("input[name=question6]:checked").val();
+    var question7 = $("input[name=question7]:checked").val();
+    var question8 = $("input[name=question8]:checked").val();
+    var question9 = $("input[name=question9]:checked").val();
+    var question10o = $("input[name=question10]:checked").val();
+
+    var result = 0;
+
+
+
+
+  }
+}
